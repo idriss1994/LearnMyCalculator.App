6ab55d3a248da07963c76c74bbb393b61aacc5b9
ait si el arabi <driss9452@hotmail.fr>
2022-05-01 18:00:58 +0000

    Add MyEventArgs class extends  Builting EventArgs

---
+++ LearnMyCalculator.App/MyEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnMyCalculator.App
+{
+    //Define event argument you want to send while raising event.
+    public class MyEventArgs : EventArgs
+    {
+        public int Value { get; set; }
+
+        public MyEventArgs(int value)
+        {
+            Value = value;
+        }
+    }
+}
