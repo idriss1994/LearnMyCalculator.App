146f2fe4e4d540b2641b7dee2f36af80fb53e877
ait si el arabi <driss9452@hotmail.fr>
2022-05-01 15:24:37 +0000

    Add Pub class to project

---
+++ LearnMyCalculator.App/Pub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnMyCalculator.App
+{
+    //Publisher class
+    public class Pub
+    {
+        //OnChange property contains all the
+        //list of subscribers callback method
+        public Action OnChange { get; set; }
+
+        public void Raise()
+        {
+            //Check if OnChange Action set before invoking it 
+            if (OnChange != null)
+            {
+                //Invoke OnChange action
+                OnChange();
+            }
+        }
+    }
+}
