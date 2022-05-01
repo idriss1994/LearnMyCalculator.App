33527e36ef6ab3b7080e114654e44bd7fd68db42
ait si el arabi <driss9452@hotmail.fr>
2022-05-01 15:40:35 +0000

    Implementation of Pub-Sub pattern in C# using Action delegate

--- LearnMyCalculator.App/Program.cs
+++ LearnMyCalculator.App/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LearnMyCalculator.App;
+using System;
 
 namespace LearnMyCalculatorApp
 {
@@ -6,7 +7,23 @@ namespace LearnMyCalculatorApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Initialize pub class  object
+            Pub p = new Pub();
+
+            //Register for OnChange event  subscriber 1
+            p.OnChange += () => Console.WriteLine("Subscriber 1!");
+
+            //Register for event subscriber 2
+            p.OnChange += () => Console.WriteLine("Subscriber 2!");
+
+            //Raise the event 
+            p.Raise();
+
+            //After this Raise() method is called
+            //all subscribers callback methods will get invoked
+
+            //Console.WriteLine("Press enter to terminate!");
+            //Console.ReadLine();
         }
     }
 }
