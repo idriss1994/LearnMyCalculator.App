8b2563aa9a855b0cbc355e7196a173928448ed29
ait si el arabi <driss9452@hotmail.fr>
2022-05-03 21:30:19 +0000

    Fix conflict in program.cs file

--- LearnMyCalculator.App/Program.cs
+++ LearnMyCalculator.App/Program.cs
@@ -6,7 +6,23 @@ namespace LearnMyCalculatorApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Initialize pub class  object
+            Pub p = new Pub();
+            p.Raise();
+            //Register for OnChange event  subscriber 1
+            p.OnChange += (sender, e) => Console.WriteLine("Subscriber 1!");
+
+            //Register for event subscriber 2
+            p.OnChange += (sender, e) => Console.WriteLine("Subscriber 2!");
+
+            //Raise the event 
+            p.Raise();
+
+            //After this Raise() method is called
+            //all subscribers callback methods will get invoked
+
+            //Console.WriteLine("Press enter to terminate!");
+            //Console.ReadLine();
         }
     }
 }
